917d3b01625773d65c51badccc33b0490c321c39
Andrey Orst <andreyorst@gmail.com>
2020-01-06 23:46:02 +0300

    prototype of eval with cons car and cdr

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -9,14 +9,14 @@ pub struct Identifier {
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Type {
-    _U32,
-    _I32,
-    _F32,
-    _Name,
-    _Str,
-    _Symbol,
-    _List,
-    _Procedure
+    U32,
+    I32,
+    F32,
+    Name,
+    Str,
+    Symbol,
+    List,
+    Procedure,
 }
 
 pub struct Evaluator {
@@ -39,49 +39,65 @@ pub enum EvalError {
 
 impl Evaluator {
     pub fn eval(&mut self, expression: &NodePtr) -> Result<NodePtr, EvalError> {
-        if !expression.borrow().childs.is_empty() {
-            let proc = match Self::car(&expression) {
-                Ok(res) => {
-                    if !res.borrow().childs.is_empty() {
-                        match self.eval(&res) {
-                            Ok(res) => res,
-                            Err(e) => return Err(e),
-                        }
-                    } else {
-                        res
-                    }
-                }
-                Err(e) => return Err(e),
-            };
-
-            let args = match Self::cdr(&expression) {
-                Ok(res) => res,
-                Err(e) => return Err(e),
-            };
-
-            self.apply(&proc, &args)
-        } else {
-            Ok(expression.clone())
-        }
-    }
-
-    fn apply(&mut self, proc: &NodePtr, args: &NodePtr) -> Result<NodePtr, EvalError> {
-        println!(
-            "apply {} to {}",
-            Self::tree_to_string(&proc),
-            Self::tree_to_string(&args)
-        );
-        match proc.borrow().data.as_ref() {
-            "quote" => Self::quote(&args),
-            _ => {
-                let args = match self.eval(&args) {
+        match Self::expression_type(expression) {
+            Type::Procedure => {
+                let proc = match Self::get_first_subexpr(expression) {
                     Ok(res) => res,
                     Err(e) => return Err(e),
                 };
+                let evaled_proc = match Self::expression_type(&proc) {
+                    Type::Procedure => match self.eval(&proc) {
+                        Ok(res) => match Self::expression_type(&res) {
+                            Type::Name => res,
+                            _ => {
+                                return Err(EvalError::Vaiv {
+                                    message: format!(
+                                        "wrong type to apply: \"{}\"",
+                                        Self::tree_to_string(&res)
+                                    ),
+                                })
+                            }
+                        },
+                        Err(e) => return Err(e),
+                    },
+                    Type::Name => proc,
+                    _ => {
+                        return Err(EvalError::Vaiv {
+                            message: format!(
+                                "wrong type to apply: \"{}\"",
+                                Self::tree_to_string(&proc)
+                            ),
+                        })
+                    }
+                };
+
+                let args = match Self::get_rest_subexpr(expression) {
+                    Ok(res) => res,
+                    Err(e) => return Err(e),
+                };
+                self.apply(&evaled_proc, &args)
+            }
+            _ => Ok(expression.clone()),
+        }
+    }
+
+    pub fn apply(&mut self, proc: &NodePtr, args: &NodePtr) -> Result<NodePtr, EvalError> {
+        match proc.borrow().data.as_ref() {
+            "quote" => Ok(args.clone()),
+            _ => {
+                let evaled_args = Tree::root("(".to_owned());
+                for sub in args.borrow().childs.iter() {
+                    match self.eval(sub) {
+                        Ok(res) => {
+                            Tree::adopt_node(&evaled_args, &res);
+                        }
+                        Err(e) => return Err(e),
+                    }
+                }
                 match proc.borrow().data.as_ref() {
-                    "cons" => Self::cons(&args),
-                    "car" => Self::car(&args),
-                    "cdr" => Self::cdr(&args),
+                    "car" => Self::car(&evaled_args),
+                    "cdr" => Self::cdr(&evaled_args),
+                    "cons" => Self::cons(&evaled_args),
                     _ => Err(EvalError::UnknownProc {
                         name: proc.borrow().data.clone(),
                     }),
@@ -150,31 +166,31 @@ impl Evaluator {
         }
     }
 
-    fn _item_type(s: &NodePtr) -> Type {
+    fn expression_type(s: &NodePtr) -> Type {
         if !s.borrow().childs.is_empty() {
             if s.borrow().childs[0].borrow().data == "quote" {
-                if !s.borrow().childs[1].borrow().childs.is_empty() {
-                     Type::_Symbol
+                if s.borrow().childs[1].borrow().childs.is_empty() {
+                    Type::Symbol
                 } else {
-                     Type::_List
+                    Type::List
                 }
             } else {
-                 Type::_Procedure
+                Type::Procedure
             }
         } else if s.borrow().data.trim().parse::<i32>().is_ok() {
-            Type::_I32
+            Type::I32
         } else if s.borrow().data.trim().parse::<u32>().is_ok() {
-            Type::_U32
+            Type::U32
         } else if s.borrow().data.trim().parse::<f32>().is_ok() {
-            Type::_F32
+            Type::F32
         } else if s.borrow().data.starts_with('"') && s.borrow().data.ends_with('"') {
-            Type::_Str
+            Type::Str
         } else {
-            Type::_Name
+            Type::Name
         }
     }
 
-    fn quote(expr: &NodePtr) -> Result<NodePtr, EvalError> {
+    fn _quote(expr: &NodePtr) -> Result<NodePtr, EvalError> {
         if expr.borrow().childs.len() > 1 {
             return Err(EvalError::WrongArgAmount {
                 procedure: "quote".to_owned(),
@@ -186,47 +202,118 @@ impl Evaluator {
     }
 
     fn car(tree: &NodePtr) -> Result<NodePtr, EvalError> {
-        // if tree.borrow().childs.len() > 1 {
-        //     return Err(EvalError::WrongArgAmount {
-        //         procedure: "car".to_owned(),
-        //         expected: 1,
-        //         fact: tree.borrow().childs.len() as u32,
-        //     });
-        // }
-        if !tree.borrow().childs.is_empty() {
-            let first = Tree::clone_tree(&tree.borrow().childs[0]);
-            Ok(first)
+        if tree.borrow().childs.len() > 1 {
+            return Err(EvalError::WrongArgAmount {
+                procedure: "car".to_owned(),
+                expected: 1,
+                fact: tree.borrow().childs.len() as u32,
+            });
+        }
+        match Self::get_first_subexpr(&tree) {
+            Ok(res) => match Self::expression_type(&res) {
+                Type::List => {
+                    let first = match Self::get_rest_subexpr(&res) {
+                        Ok(res) => match Self::get_first_subexpr(&res) {
+                            Ok(res) => match Self::get_first_subexpr(&res) {
+                                Ok(res) => res,
+                                Err(e) => return Err(e),
+                            },
+                            Err(e) => return Err(e),
+                        },
+                        Err(e) => return Err(e),
+                    };
+                    match Self::expression_type(&first) {
+                        Type::Name => {
+                            let root = Tree::root("(".to_owned());
+                            Tree::add_child(&root, "quote".to_owned());
+                            Tree::adopt_node(&root, &first);
+                            Ok(root)
+                        }
+                        _ => Ok(first.clone()),
+                    }
+                }
+                _ => Err(EvalError::Vaiv {
+                    message: format!("car: expected pair, got {}", Self::tree_to_string(&res)),
+                }),
+            },
+            Err(e) => Err(e),
+        }
+    }
+
+    fn get_first_subexpr(expression: &NodePtr) -> Result<NodePtr, EvalError> {
+        if !expression.borrow().childs.is_empty() {
+            Ok(Tree::clone_tree(&expression.borrow().childs[0]))
         } else {
             Err(EvalError::Vaiv {
-                message: format!("car: expected pair, got \"{}\"", Self::tree_to_string(tree)),
+                message: format!(
+                    "car: expected pair, got \"{}\"",
+                    Self::tree_to_string(expression)
+                ),
             })
         }
     }
 
     fn cdr(tree: &NodePtr) -> Result<NodePtr, EvalError> {
-        // if tree.borrow().childs.len() > 1 {
-        //     return Err(EvalError::WrongArgAmount {
-        //         procedure: "cdr".to_owned(),
-        //         expected: 1, fact: tree.borrow().childs.len() as u32,
-        //     });
-        // }
-        if !tree.borrow().childs.is_empty() {
-            let rest =
-                if tree.borrow().childs.len() > 2 && tree.borrow().childs[1].borrow().data == "." {
-                    Tree::clone_tree(&tree.borrow().childs[2])
-                } else {
-                    let rest = Tree::clone_tree(tree);
-                    rest.borrow_mut().childs.remove(0);
-                    rest
-                };
-            Ok(rest)
-        } else {
-            Err(EvalError::Vaiv {
-                message: format!("cdr: expected pair, got \"{}\"", Self::tree_to_string(tree)),
-            })
+        if tree.borrow().childs.len() > 1 {
+            return Err(EvalError::WrongArgAmount {
+                procedure: "cdr".to_owned(),
+                expected: 1,
+                fact: tree.borrow().childs.len() as u32,
+            });
+        }
+        match Self::get_first_subexpr(&tree) {
+            Ok(res) => match Self::expression_type(&res) {
+                Type::List => {
+                    let rest = match Self::get_rest_subexpr(&res) {
+                        Ok(res) => match Self::get_first_subexpr(&res) {
+                            Ok(res) => match Self::get_rest_subexpr(&res) {
+                                Ok(res) => res,
+                                Err(e) => return Err(e),
+                            },
+                            Err(e) => return Err(e),
+                        },
+                        Err(e) => return Err(e),
+                    };
+                    match Self::expression_type(&rest) {
+                        Type::Procedure => {
+                            let root = Tree::root("(".to_owned());
+                            Tree::add_child(&root, "quote".to_owned());
+                            Tree::adopt_node(&root, &rest);
+                            Ok(root)
+                        }
+                        _ => Ok(rest.clone()),
+                    }
+                }
+                _ => Err(EvalError::Vaiv {
+                    message: format!("cdr: expected pair, got {}", Self::tree_to_string(&res)),
+                }),
+            },
+            Err(e) => Err(e),
         }
     }
 
+    fn get_rest_subexpr(expression: &NodePtr) -> Result<NodePtr, EvalError> {
+        if !expression.borrow().childs.is_empty() {
+            Ok(
+                if expression.borrow().childs.len() > 2
+                    && expression.borrow().childs[1].borrow().data == "."
+                {
+                    Tree::clone_tree(&expression.borrow().childs[2])
+                } else {
+                    let rest = Tree::clone_tree(expression);
+                    rest.borrow_mut().childs.remove(0);
+                    rest
+                },
+            )
+        } else {
+            Err(EvalError::Vaiv {
+                message: format!(
+                    "cdr: expected pair, got \"{}\"",
+                    Self::tree_to_string(expression)
+                ),
+            })
+        }
+    }
     fn cons(args: &NodePtr) -> Result<NodePtr, EvalError> {
         if args.borrow().childs.len() != 2 {
             return Err(EvalError::WrongArgAmount {
@@ -235,22 +322,41 @@ impl Evaluator {
                 fact: args.borrow().childs.len() as u32,
             });
         }
-
-        let root = Tree::root("(".to_owned());
-        let first = match Self::car(&args) {
-            Ok(res) => res,
+        let first = match Self::get_first_subexpr(&args) {
+            Ok(res) => match Self::expression_type(&res) {
+                Type::List | Type::Symbol => match Self::get_rest_subexpr(&res) {
+                    Ok(res) => match Self::get_first_subexpr(&res) {
+                        Ok(res) => res,
+                        Err(e) => return Err(e),
+                    },
+                    Err(e) => return Err(e),
+                },
+                _ => res,
+            },
             Err(e) => return Err(e),
         };
-        let second = match Self::cdr(&args) {
-            Ok(res) => match Self::car(&res) {
-                Ok(res) => res,
+        let second = match Self::get_rest_subexpr(args) {
+            Ok(res) => match Self::get_first_subexpr(&res) {
+                Ok(res) => match Self::expression_type(&res) {
+                    Type::List | Type::Symbol => match Self::get_rest_subexpr(&res) {
+                        Ok(res) => match Self::get_first_subexpr(&res) {
+                            Ok(res) => res,
+                            Err(e) => return Err(e),
+                        },
+                        Err(e) => return Err(e),
+                    },
+                    _ => res,
+                },
                 Err(e) => return Err(e),
             },
             Err(e) => return Err(e),
         };
-        Tree::adopt_node(&root, &first);
-        Tree::add_child(&root, ".".to_owned());
-        Tree::adopt_node(&root, &second);
+        let root = Tree::root("(".to_owned());
+        Tree::add_child(&root, "quote".to_owned());
+        let root2 = Tree::add_child(&root, "(".to_owned());
+        Tree::adopt_node(&root2, &first);
+        Tree::add_child(&root2, ".".to_owned());
+        Tree::adopt_node(&root2, &second);
         if Parser::remove_dots(&root).is_err() {
             return Err(EvalError::Vaiv {
                 message: "vaiv".to_owned(),
@@ -263,10 +369,40 @@ impl Evaluator {
 #[test]
 fn test_types() {
     let mut parser = Parser::new();
-    assert_eq!(Evaluator::_item_type(&parser.parse("32").ok().unwrap().borrow().childs[0]), Type::_I32);
-    assert_eq!(Evaluator::_item_type(&parser.parse("-32").ok().unwrap().borrow().childs[0]), Type::_I32);
-    assert_eq!(Evaluator::_item_type(&parser.parse("32.0").ok().unwrap().borrow().childs[0]), Type::_F32);
-    assert_eq!(Evaluator::_item_type(&parser.parse("-32.0").ok().unwrap().borrow().childs[0]), Type::_F32);
-    assert_eq!(Evaluator::_item_type(&parser.parse("\"str\"").ok().unwrap().borrow().childs[0]), Type::_Str);
-    assert_eq!(Evaluator::_item_type(&parser.parse("name").ok().unwrap().borrow().childs[0]), Type::_Name);
+    assert_eq!(
+        Evaluator::expression_type(&parser.parse("32").ok().unwrap().borrow().childs[0]),
+        Type::I32
+    );
+    assert_eq!(
+        Evaluator::expression_type(&parser.parse("-32").ok().unwrap().borrow().childs[0]),
+        Type::I32
+    );
+    assert_eq!(
+        Evaluator::expression_type(&parser.parse("32.0").ok().unwrap().borrow().childs[0]),
+        Type::F32
+    );
+    assert_eq!(
+        Evaluator::expression_type(&parser.parse("-32.0").ok().unwrap().borrow().childs[0]),
+        Type::F32
+    );
+    assert_eq!(
+        Evaluator::expression_type(&parser.parse("\"str\"").ok().unwrap().borrow().childs[0]),
+        Type::Str
+    );
+    assert_eq!(
+        Evaluator::expression_type(&parser.parse("name").ok().unwrap().borrow().childs[0]),
+        Type::Name
+    );
+    assert_eq!(
+        Evaluator::expression_type(&parser.parse("'name").ok().unwrap().borrow().childs[0]),
+        Type::Symbol
+    );
+    assert_eq!(
+        Evaluator::expression_type(&parser.parse("(name)").ok().unwrap().borrow().childs[0]),
+        Type::Procedure
+    );
+    assert_eq!(
+        Evaluator::expression_type(&parser.parse("'(name)").ok().unwrap().borrow().childs[0]),
+        Type::List
+    );
 }
