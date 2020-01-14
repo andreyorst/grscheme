3e23ad915f53124ca27952919443d64cf53a9f6c
Andrey Orst <andreyorst@gmail.com>
2020-01-14 10:21:21 +0300

    execute file passed as parameter

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,34 @@ mod evaluator;
 mod parser;
 mod repl;
 mod tree;
+use std::env;
+use std::fs;
+use std::process;
+use crate::parser::Parser;
+use crate::evaluator::Evaluator;
 
 fn main() {
-    repl::run();
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() == 1 {
+        let file = args[0].clone();
+        let contents = match fs::read_to_string(file) {
+            Ok(c) => c,
+            Err(e) => {
+                println!("error {}", e);
+                process::exit(1);
+            }
+        };
+        let mut parser = Parser::new();
+        let res = match parser.parse(&contents) {
+            Ok(t) => t,
+            Err(_) => process::exit(2),
+        };
+        let mut evaluator = Evaluator::new();
+        match evaluator.eval(&res) {
+            Ok(res) => Evaluator::print(&res),
+            Err(_) => process::exit(3),
+        }
+    } else if args.is_empty() {
+        repl::run();
+    }
 }
